f68c1469a3877e31a716cbfd51427a8b23150ca4
howardchi <howardchi@fougood.com>
2016-10-09 19:21:46 +0800

    initial tests

--- docs/index.js
+++
@@ -1,9 +0,0 @@
-/* eslint-disable */
-import React from 'react';
-import ReactDOM from 'react-dom';
-import Plugins from '../src';
-console.log(Plugins)
-
-ReactDOM.render(
-  <div/>
-, document.getElementById('root'));
--- test/slate-plugins-test.js
+++ test/slate-plugins-test.js
@@ -1,9 +1,37 @@
-/* eslint-disable */
-import assert from 'assert';
-import PackageName from '../src';
+const expect = require('expect');
+const fs = require('fs');
+const path = require('path');
+const Slate = require('slate');
+const readMetadata = require('read-metadata');
 
-describe('PackageName', function() {
-  it('should have unit test!', function() {
-    assert(false, 'we expected this package author to add actual unit tests.');
+describe('slate-plugins test', function() {
+  const tests = fs.readdirSync(__dirname);
+
+  tests.forEach(function(test) {
+    if (test[0] === '.' || path.extname(test).length > 0) {
+      return;
+    }
+
+    it(test, function() {
+      const dir = path.resolve(__dirname, test);
+
+      const inputPath = path.resolve(dir, 'input.yaml');
+      const input = readMetadata.sync(inputPath);
+
+      const expectedPath = path.resolve(dir, 'expected.yaml');
+      let expected;
+      if (fs.existsSync(expectedPath)) {
+        expected = readMetadata.sync(expectedPath);
+      }
+
+      const runTransform = require(path.resolve(dir, 'transform.js'));
+      const stateInput = Slate.Raw.deserialize(input, {terse: true});
+      const newState = runTransform(stateInput);
+
+      if (expected) {
+        const newDocJSon = Slate.Raw.serialize(newState, {terse: true});
+        expect(newDocJSon).toEqual(expected);
+      }
+    });
   });
 });
