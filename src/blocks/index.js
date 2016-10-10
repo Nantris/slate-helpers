61e6923e48b9c74f60cf78c5eb0ec41f90cc763b
howardchi <howardchi@fougood.com>
2016-10-10 11:16:00 +0800

    fix export naming

--- src/blocks/index.js
+++ src/blocks/index.js
@@ -2,6 +2,6 @@ import heading from './heading';
 import blockquote from './blockquote';
 import insertBlock from './insertBlock';
 import insertTable from './insertTable';
-import setListBlock from './setListBlock';
+import List from './list';
 
-export default {heading, blockquote, insertTable, insertBlock, setListBlock};
+export default {heading, blockquote, insertTable, insertBlock, List};
