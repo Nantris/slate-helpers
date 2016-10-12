408607fa54c6d67e1f858ad4084ee4bde6787333
howardchi <howardchi@fougood.com>
2016-10-12 10:30:00 +0800

    add export history

--- src/index.js
+++ src/index.js
@@ -2,10 +2,12 @@ import blocks from './blocks';
 import inlines from './inlines';
 import marks from './marks';
 import utils from './utils';
+import history from './history';
 
 export default {
   blocks,
   inlines,
   marks,
-  utils
+  utils,
+  history
 };
