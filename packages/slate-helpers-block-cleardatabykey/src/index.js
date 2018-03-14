78651be05f91565c499655df60e4942fe66d8204
chilijung <chilijung@gmail.com>
2018-03-14 12:28:32 +0800

    add cleardatabykey package

--- packages/slate-helpers-block-cleardatabykey/src/index.js
+++ packages/slate-helpers-block-cleardatabykey/src/index.js
@@ -2,23 +2,24 @@
  * this delete a data key in current block type
  *
  * @param {Slate.state} state
- * @param {Datakey} key
+ * @param {Datakey} dataKey
  * @return {Slate.state}
 **/
-export default (state, key) => {
-  let newState = state.transform();
+export default (change, dataKey) => {
+  const {value} = change;
+  const blocks = value.document.getBlocks();
 
   // if have blocks
-  if (state.blocks) {
-    state.blocks.forEach(type => {
+  if (blocks) {
+    blocks.forEach(type => {
       const typeOriginalData = type.get('data');
-      const newData = typeOriginalData.delete(key);
+      const newData = typeOriginalData.delete(dataKey);
 
       const newType = type.set('data', newData);
       // reset current type with new data
-      newState.setBlock(newType);
+      change.setBlocks(newType);
     });
   }
 
-  return newState.apply();
+  return change;
 };
