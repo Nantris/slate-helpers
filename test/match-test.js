9a2a7c774115f26dfa46bab6fa4903a441e5efab
chilijung <chilijung@gmail.com>
2018-03-14 12:01:09 +0800

    change to monorepo

--- test/match-test.js
+++ test/match-test.js
@@ -1,6 +1,5 @@
 import expect from 'expect';
 import fs from 'fs';
-import path from 'path';
 import Slate from 'slate';
 import readMetadata from 'read-metadata';
 
@@ -11,27 +10,17 @@ function deserializeValue(json) {
   );
 }
 
-describe('slate-plugins test', () => {
-  const tests = fs.readdirSync(__dirname);
-
-  tests.forEach(test => {
-    if (test[0] === '.' || path.extname(test).length > 0) {
-      return;
-    }
-
-    it(test, () => {
-      const dir = path.resolve(__dirname, test);
-
-      const inputPath = path.resolve(dir, 'input.yaml');
+export default function(name, inputPath, expectedPath, transformPath) {
+  describe(name, () => {
+    it('match input and expected yaml', () => {
       const input = readMetadata.sync(inputPath);
 
-      const expectedPath = path.resolve(dir, 'expected.yaml');
       let expected;
       if (fs.existsSync(expectedPath)) {
         expected = readMetadata.sync(expectedPath);
       }
 
-      const runTransform = require(path.resolve(dir, 'transform.js')).default;
+      const runTransform = require(transformPath).default;
       const valueInput = deserializeValue(input);
       const newChange = runTransform(valueInput.change());
 
@@ -41,4 +30,4 @@ describe('slate-plugins test', () => {
       }
     });
   });
-});
+}
