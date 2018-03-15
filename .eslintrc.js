12ddf2d0cd69bbe89a2e9c6308ba7b6083c121df
chilijung <chilijung@gmail.com>
2018-03-15 11:18:14 +0800

    add prettier

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,6 +1,6 @@
 module.exports = {
-  extends: ["eslint:recommended", "plugin:react/recommended"],
-  plugins: ["react", "flowtype"],
+  extends: ["eslint:recommended", "plugin:react/recommended", "prettier"],
+  plugins: ["react", "flowtype", "prettier"],
   env: {
     browser: true,
     node: true
