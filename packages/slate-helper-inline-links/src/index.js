0dcaa590d0ee9338acdab384e54a34005af7c84c
chilijung <chilijung@gmail.com>
2018-03-19 16:23:44 +0800

    lint

--- packages/slate-helper-inline-links/src/index.js
+++ packages/slate-helper-inline-links/src/index.js
@@ -1,13 +1,14 @@
 import { haveInlines } from "@canner/slate-util-have";
 
 export const DEFAULT = {
-  href: "http://example.com/",
-  text: "example link"
+  href: "http://example.com/"
 };
 
 export default (change, type, opt = DEFAULT) => {
-  const { text, href } = Object.assign({}, DEFAULT, opt);
+  let { text, href } = Object.assign({}, DEFAULT, opt);
   let haveLinks = haveInlines(change, type);
+
+  if (!text) text = href;
 
   if (haveLinks) {
     change.unwrapInline(type);
